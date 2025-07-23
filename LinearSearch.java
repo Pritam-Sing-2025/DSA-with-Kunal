caf5de4afe82f97cb05da5f49a00e13682c02d01
Pritam-Sing-2025 <pritamsing2025@gmail.com>
2025-07-23 12:21:45 +0530

    Add file

---
+++ LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {14, 53, 32, 29, 60, 70, 38};
+        int target = 29;
+        System.out.println(linearSearch(arr, target));
+    }
+    static int linearSearch (int []arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
